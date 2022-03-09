5aad9c378306a192deb202992e2fad2eb3b88324
capmahin <mahinulhuque1996@gmail.com>
2022-03-10 05:00:10 +0600

    start a new site called tempareture

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const API_KEY = `579b6cfac20f23a4d6b79f431e275be1`;
+const searchTemperature = () => {
+  const city = document.getElementById("city-name").value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  //   console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
